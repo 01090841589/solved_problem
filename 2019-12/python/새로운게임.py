a0ca69abae08343c23a6284c7c2b55267b930eb3
01090841589 <chanchanhwan@naver.com>
2019-12-31 00:06:48 +0900

    20191231

--- 2019-12/python/새로운게임.py
+++ 2019-12/python/새로운게임.py
@@ -1,7 +1,13 @@
 import sys
 sys.stdin = open("새로운게임.txt")
 
+DIR = [[], [-1, 0], [0, 1], [1, 0], [0, -1]]
 N, K = map(int, input().split())
 MAP = [list(map(int, input().split())) for _ in range(N)]
 dron = [list(map(int, input().split())) for _ in range(K)]
 print(MAP, dron)
+
+for _ in range(K):
+    for i in range(4):
+
+        
