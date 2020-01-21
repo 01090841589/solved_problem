4f351447e8bdc5dc71a2bcd574feaf282926b69c
박찬환 <chanchanhwan@naver.com>
2020-01-22 08:58:12 +0900

    20200122

--- 2020-01/python/00001_전략게임.py
+++ 2020-01/python/00001_전략게임.py
@@ -1,13 +1,15 @@
 import sys
 sys.stdin = open("전략게임.txt")
 
+from collections import deque
 
 T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
     MAP = [list(map(int, input().split())) for _ in range(N)]
     A, B, C = map(int, input().split())
-    Acode = list(map(int, input().split()))
-    Bcode = list(map(int, input().split()))
-    Ccode = list(map(int, input().split()))
+    Acode = [list(map(int, input().split())) for _ in range(A)]
+    Bcode = [list(map(int, input().split())) for _ in range(B)]
+    Ccode = [list(map(int, input().split())) for _ in range(C)]
     code = []
+    q = deque()
