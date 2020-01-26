94b0e2cacd7053f8b9a462b0c5dd042b056ba41d
01090841589 <chanchanhwan@naver.com>
2020-01-27 01:47:49 +0900

    20200127

--- 2020-01/python/00001_전략게임.py
+++ 2020-01/python/00001_전략게임.py
@@ -8,6 +8,7 @@ T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
     MAP = [list(map(int, input().split())) for _ in range(N)]
+    visited = [list(map(int, input().split())) for _ in range(N)]
     A, B, C = map(int, input().split())
     Acode = [list(map(int, input().split())) for _ in range(A)]
     Bcode = [list(map(int, input().split())) for _ in range(B)]
