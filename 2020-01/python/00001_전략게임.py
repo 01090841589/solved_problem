1fc5258c6de63c91e93794fb44c09660dab01cde
박찬환 <chanchanhwan@naver.com>
2020-01-28 08:57:55 +0900

    20200128

--- 2020-01/python/00001_전략게임.py
+++ 2020-01/python/00001_전략게임.py
@@ -1,20 +1,21 @@
 import sys
 sys.stdin = open("전략게임.txt")
 
-DIR = [[0, 1], [1, 0], [0, -1]]
+DIR = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
 from collections import deque
 
 T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
     MAP = [list(map(int, input().split())) for _ in range(N)]
-    visited = [list(map(int, input().split())) for _ in range(N)]
+    visited = [[0] * 16 for _ in range(M) for _ in range(N)]
     A, B, C = map(int, input().split())
     Acode = [list(map(int, input().split())) for _ in range(A)]
     Bcode = [list(map(int, input().split())) for _ in range(B)]
     Ccode = [list(map(int, input().split())) for _ in range(C)]
     code = []
-    q = deque([0, 0, [], 0])
+    q = deque([[0, 0, [0, 0, 0, 0], 0]])
     while q:
         y, x, code, k = q.popleft()
         for c in DIR:
