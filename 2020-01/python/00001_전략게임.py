d1d4eee46f0d836ddebe5de8f950806de4cac461
박찬환 <chanchanhwan@naver.com>
2020-01-29 17:58:35 +0900

    20200129

--- 2020-01/python/00001_전략게임.py
+++ 2020-01/python/00001_전략게임.py
@@ -21,7 +21,7 @@ for tc in range(1, T+1):
     for _ in range(C):
         password = list(map(int, input().split()))
         CodeActive[password[0]*8+password[1]*4+password[2]*2+password[3]] = 3
-    result = 999999
+    result = 100
     q = deque([[0, 0, 0, 0, 0, 0]])
     while q:
         y, x, code, k, bonus, step = q.popleft()
@@ -35,30 +35,36 @@ for tc in range(1, T+1):
             X = x+c[1]
             if 0 <= Y < N and 0 <= X < N:
                 check = ((code << 1)+MAP[Y][X])%16
-                if CodeActive[check] == 1 and step > 3:
+                if CodeActive[check] == 1 and step >= 3:
                     check = MAP[0][0]
                     if k + 1 - bonus < visited[0][0][check]:
-                        visited[0][0][check] = k + 1 - bonus
+                        if step >= 3:
+                            visited[0][0][check] = k + 1 - bonus
                         q.append([0, 0, check, k+1 - bonus, 0, 1])
-                elif CodeActive[check] == 2 and step > 3:
+                elif CodeActive[check] == 2 and step >= 3:
                     check = 0
                     if k + 1 - bonus < visited[(N//2)-1][(N//2)-1][check]:
-                        visited[(N // 2) - 1][(N // 2) - 1][check] = k + 1 - bonus
+                        if step >= 3:
+                            visited[(N // 2) - 1][(N // 2) - 1][check] = k + 1 - bonus
                         q.append([(N//2)-1, (N//2)-1, check, k+1 - bonus, 0, 0])
-                elif CodeActive[check] == 3 and step > 3:
+                elif CodeActive[check] == 3 and step >= 3:
                     check = 0
                     if k + 1 - bonus < visited[Y][X][check]:
-                        visited[Y][X][check] = k + 1 - bonus
-                        q.append([Y, X, check, k+1 - bonus, 1, 0])
+                        if step >= 3:
+                            visited[Y][X][check] = k + 1 - bonus
+                        q.appendleft([Y, X, check, k+1 - bonus, 1, 0])
                 else:
                     if k + 1 - bonus < visited[Y][X][check]:
-                        visited[Y][X][check] = k + 1 - bonus
+                        if step >= 3:
+                            visited[Y][X][check] = k + 1 - bonus
                         q.append([Y, X, check, k+1 - bonus, 0, step+1])
-    for y in range(N):
-        for x in range(N):
-            print(visited[y][x][0], end=' ')
-        print()
-    if result != 999999:
+    # for z in range(16):
+    #     for y in range(N):
+    #         for x in range(N):
+    #             print(visited[y][x][z], end=' ')
+    #         print()
+    #     print("{}--------------------------------------------".format(z))
+    if result != 100:
         print('#{} {}'.format(tc, result))
     else:
         print('#{} -1'.format(tc))
