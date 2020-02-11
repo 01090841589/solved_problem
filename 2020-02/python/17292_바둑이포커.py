199acd7810ca267f8f09fa56e1657444d73bb64b
박찬환 <chanchanhwan@naver.com>
2020-02-11 09:09:15 +0900

    20200211

--- 2020-02/python/17292_바둑이포커.py
+++ 2020-02/python/17292_바둑이포커.py
@@ -1,2 +1,11 @@
 import sys
 sys.stdin = open("바둑이포커.txt")
+
+cards = list(input().split(','))
+
+print(cards)
+for i in range(6):
+    for j in range(i, 6):
+        if i != j:
+
+            print(cards[i], cards[j])
