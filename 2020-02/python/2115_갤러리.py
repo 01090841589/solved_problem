ac95e6a05cd89c50af9359a7a34ee0021a85c390
01090841589 <chanchanhwan@naver.com>
2020-02-21 20:20:21 +0900

    20200221

--- 2020-02/python/2115_갤러리.py
+++ 2020-02/python/2115_갤러리.py
@@ -3,5 +3,7 @@ sys.stdin = open("갤러리.txt")
 
 N, M = map(int, input().split())
 MAP = [map(int, input().split()) for _ in range(N)]
-for i in range(N):
-    pass
+for y in range(N):
+    for x in range(M):
+
+        pass
