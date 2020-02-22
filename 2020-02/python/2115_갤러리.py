024cf04a0f4d6aa2200da312dd730e109da88890
01090841589 <chanchanhwan@naver.com>
2020-02-22 21:29:48 +0900

    20200222

--- 2020-02/python/2115_갤러리.py
+++ 2020-02/python/2115_갤러리.py
@@ -5,5 +5,7 @@ N, M = map(int, input().split())
 MAP = [map(int, input().split()) for _ in range(N)]
 for y in range(N):
     for x in range(M):
+        if MAP[y][x] == 'X':
+            print(1)
 
         pass
