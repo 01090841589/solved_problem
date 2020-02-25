26b72f9196824a8dfbde2f4ae358cd8b1ea53eb8
01090841589 <chanchanhwan@naver.com>
2020-02-25 16:04:28 +0900

    20200225

--- 2020-02/python/2115_갤러리.py
+++ 2020-02/python/2115_갤러리.py
@@ -1,14 +1,72 @@
 import sys
 sys.stdin = open("갤러리.txt")
 
-def four():
-    pass
-
 N, M = map(int, input().split())
-MAP = [map(int, input().split()) for _ in range(N)]
-for y in range(N):
+MAP = [list(input()) for _ in range(N)]
+res = 0
+flag = 1
+for y in range(N - 1):
     for x in range(M):
         if MAP[y][x] == 'X':
-            print(1)
-            four()
-        pass
+            if flag:
+                if MAP[y+1][x] == '.':
+                    flag = 0
+                else:
+                    flag = 1
+            else:
+                if MAP[y+1][x] == '.':
+                    flag = 1
+                    res += 1
+                else:
+                    flag = 1
+        else:
+            flag = 1
+for y in range(1, N):
+    for x in range(M):
+        if MAP[y][x] == 'X':
+            if flag:
+                if MAP[y-1][x] == '.':
+                    flag = 0
+                else:
+                    flag = 1
+            else:
+                if MAP[y-1][x] == '.':
+                    flag = 1
+                    res += 1
+                else:
+                    flag = 1
+        else:
+            flag = 1
+for x in range(M - 1):
+    for y in range(N):
+        if MAP[y][x] == 'X':
+            if flag:
+                if MAP[y][x+1] == '.':
+                    flag = 0
+                else:
+                    flag = 1
+            else:
+                if MAP[y][x+1] == '.':
+                    flag = 1
+                    res += 1
+                else:
+                    flag = 1
+        else:
+            flag = 1
+for x in range(1, M):
+    for y in range(N):
+        if MAP[y][x] == 'X':
+            if flag:
+                if MAP[y][x-1] == '.':
+                    flag = 0
+                else:
+                    flag = 1
+            else:
+                if MAP[y][x-1] == '.':
+                    flag = 1
+                    res += 1
+                else:
+                    flag = 1
+        else:
+            flag = 1
+print(res)
