3eb460ece2a4f72daf04b68ba45f4d72c2d79809
01090841589 <chanchanhwan@naver.com>
2020-02-25 15:46:03 +0900

    20200225

--- 2020-02/python/2115_갤러리.py
+++ 2020-02/python/2115_갤러리.py
@@ -1,10 +1,14 @@
 import sys
 sys.stdin = open("갤러리.txt")
 
+def four():
+    pass
+
 N, M = map(int, input().split())
 MAP = [map(int, input().split()) for _ in range(N)]
 for y in range(N):
     for x in range(M):
         if MAP[y][x] == 'X':
             print(1)
+            four()
         pass
