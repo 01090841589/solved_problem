6422e478d607c202aaad2eb4edbcf0defdb385c0
박찬환 <chanchanhwan@naver.com>
2020-04-23 00:43:35 +0900

    20200423

---
+++ 2020-04/17430_가로등.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.stdin = open("가로등.txt")
+
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    road = [[0]*N for _ in range(N)]
+    for i in range(N):
+        a, b = map(int, input().split())
+        
+    print(road)
---
+++ 2020-04/18351_특정거리의도시찾기.py
@@ -0,0 +1,28 @@
+import sys
+sys.stdin = open("특정거리.txt")
+
+from collections import deque
+
+N, M, K, X = map(int, input().split())
+visited = [300001]*(N)
+MAP = [[] for _ in range(N)]
+res = []
+for road in range(M):
+    a, b = map(int, input().split())
+    MAP[a-1].append(b-1)
+q = deque()
+q.append(X-1)
+visited[X-1] = 0
+while q:
+    site = q.popleft()
+    for go in MAP[site]:
+        if visited[site]+1 < visited[go]:
+            q.append(go)
+            visited[go] = visited[site]+1
+            if visited[go] == K:
+                res.append(go)
+if res:
+    res.sort()
+    [print(i+1) for i in res]
+else:
+    print(-1)
