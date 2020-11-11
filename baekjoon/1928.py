d03b648378f48e191f02ff21a0458dcf9d55b2c6
01090841589 <chanchanhwan@naver.com>
2020-11-11 23:23:12 +0900

    20201111

--- baekjoon/1928.py
+++ baekjoon/1928.py
@@ -1,13 +1,81 @@
-def dd(su, su2):
-    if (su > 0 and su2 < 0) or (su < 0 and su2 > 0):
-        if su % su2:
-            return su // su2 + 1
-        else:
-            return su // su2
-    else:
-        return su // su2
+import sys
+sys.stdin = open("1938.txt")
 
-print(dd(16, -3))
-print(dd(-16, 3))
-print(dd(16, 3))
-print(dd(-16, -3))
+sik = list(input())
+
+def det_oper(a,su,su2):
+    if a == '+':
+        return su+su2
+    elif a == '-':
+        return su-su2
+    elif a == '*':
+        return su*su2
+    else:
+        if (su >0 and su2 <0) or (su <0 and su2 > 0):
+            if su%su2:
+                return su//su2+1
+            else: return su//su2
+        else:
+            return su//su2
+
+if sik[0] == '-':
+    f = 1
+    m = -1
+else: f = 0; m = 1
+
+numbers = []
+opers = []
+tem = ''
+for i in range(f,len(sik)):
+    if sik[i] in '1234567890':
+        tem += sik[i]
+    else:
+        if len(numbers) == 0:
+            numbers.append(m*int(tem))
+        else:
+            numbers.append(int(tem))
+        tem = ''
+        opers.append(sik[i])
+numbers.append(int(tem))
+
+front, back = 0, len(numbers)-1
+f,b = 0, len(opers)-1
+flag = True
+
+num1, num2 = numbers[front], numbers[back]
+
+if len(opers) == 0:
+    ans = num1
+else:
+    while flag :
+        oper1, oper2 = opers[f], opers[b]
+
+        if f == b:
+            ans = det_oper(oper1,num1,num2)
+            flag = False
+        else:
+            if oper1 in '*/' and oper2 in '+-':
+                tem = numbers[front+1]
+                num1 = det_oper(oper1,num1,tem)
+                front +=1
+                f +=1
+            elif oper1 in '+-' and oper2 in '*/':
+                tem = numbers[back-1]
+                num2 = det_oper(oper1,tem,num2)
+                back -=1
+                b -=1
+            else:
+                tem = numbers[front+1]
+                tem2 = numbers[back-1]
+                tem_num1 = det_oper(oper1,num1,tem)
+                tem_num2 = det_oper(oper2,tem2,num2)
+                if tem_num1 >= tem_num2:
+                    num1 = tem_num1
+                    front +=1
+                    f += 1
+                else:
+                    num2 = tem_num2
+                    back -= 1
+                    b -= 1
+
+print(ans)
--- baekjoon/1938.py
+++ baekjoon/1938.py
@@ -1,81 +1,71 @@
 import sys
 sys.stdin = open("1938.txt")
-
 from collections import deque
 
-def find_tree(tree):
-    for y in range(N):
-        for x in range(N):
-            if MAP[y][x] == tree:
-                if 0 <= y+1 < N and MAP[y+1][x] == tree:
-                    return y, x, 0
-                elif 0 <= x+1 < N and MAP[y][x+1] == tree:
-                    return y, x, 1
+orders = input()
+minus, start = 1, 0
+nums = deque()
+oper = deque()
+operation = ['+', '*', '-', '/']
+buf = ''
 
-def check_space(y, x, arr):
-    if not arr and 0 <= x-1 and x+1 < N:
-        for Y in range(y, y+3):
-            if MAP[Y][x-1] or MAP[Y][x+1]:
-                break
+
+def calculate(num1, num2, oper):
+    if oper == '+':
+        return num1 + num2
+    if oper == '-':
+        return num1 - num2
+    if oper == '*':
+        return num1 * num2
+    if oper == '/':
+        if num1 * num2 >= 0:
+            return num1 // num2
         else:
-            return True
-    elif arr and 0 <= y-1 and y+1 < N:
-        for X in range(x, x+3):
-            if MAP[y+1][X] or MAP[y-1][X]:
-                break
-        else:
-            return True
-    return False
-
-def rotate_tree(y, x, arr):
-    if arr:
-        return y-1, x+1, arr-1
-    else:
-        return y+1, x-1, arr+1
-
-
-DIR = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-N = int(input())
-MAP = [list(input()) for _ in range(N)]
-moves = [[[N**3, N**3] for _ in range(N)] for _ in range(N)]
-y, x, arr = find_tree('B')
-des_y, des_x, des_arr = find_tree('E')
-moves[y][x][arr] = 0
-q = deque()
-q.append([y, x, arr, 0])
-
-for y in range(N):
-    for x in range(N):
-        if MAP[y][x] == '1':
-            MAP[y][x] = 1
-        else:
-            MAP[y][x] = 0
-while q:
-    y, x, arr, scr = q.popleft()
-    if moves[des_y][des_x][des_arr] <= scr:
-        continue
-    flag = check_space(y, x, arr)
-    if flag:
-        ro_y, ro_x, ro_arr = rotate_tree(y, x, arr)
-        if moves[ro_y][ro_x][ro_arr] > scr+1:
-            moves[ro_y][ro_x][ro_arr] = scr+1
-            q.append([ro_y, ro_x, ro_arr, scr+1])
-    for c in DIR:
-        Y = y+c[0]
-        X = x+c[1]
-        if 0 <= Y < N and 0 <= X < N:
-            if arr:
-                if X+2 < N and not MAP[Y][X] and not MAP[Y][X+1] and not MAP[Y][X+2]:
-                    if moves[Y][X][arr] > scr+1:
-                        moves[Y][X][arr] = scr + 1
-                        q.append([Y, X, arr, scr+1])
+            if abs(num1 % num2):
+                return (num1 // num2) + 1
             else:
-                if Y+2 < N and not MAP[Y][X] and not MAP[Y+1][X] and not MAP[Y+2][X]:
-                    if moves[Y][X][arr] > scr+1:
-                        moves[Y][X][arr] = scr + 1
-                        q.append([Y, X, arr, scr+1])
-res = moves[des_y][des_x][des_arr]
-if res == N**3:
-    print(0)
+                return (num1 // num2)
+
+
+if orders[0] == '-':
+    minus = -1
+    start = 1
+for i in range(start, len(orders)):
+    if '0' <= orders[i] <= '9':
+        buf += orders[i]
+    else:
+        nums.append(int(buf))
+        oper.append(orders[i])
+        buf = ''
+nums.append(int(buf))
+nums[0] *= minus
+while len(oper) > 1:
+    left = (operation.index(oper[0])) % 2
+    right = (operation.index(oper[-1])) % 2
+    if left > right:
+        num = calculate(nums.popleft(), nums.popleft(), oper.popleft())
+        nums.appendleft(num)
+    elif left < right:
+        num = calculate(nums[-2], nums[-1], oper.pop())
+        nums.pop()
+        nums.pop()
+        nums.append(num)
+    else:
+        left_cal = calculate(nums[0], nums[1], oper[0])
+        right_cal = calculate(nums[-2], nums[-1], oper[-1])
+        if left_cal >= right_cal:
+            nums.popleft()
+            nums.popleft()
+            nums.appendleft(left_cal)
+            oper.popleft()
+        else:
+            nums.pop()
+            nums.pop()
+            nums.append(right_cal)
+            oper.pop()
+    print(nums, oper)
+if len(oper):
+    res = calculate(nums[0], nums[1], oper[0])
 else:
-    print(res)
+    res = nums[0]
+print(res)
